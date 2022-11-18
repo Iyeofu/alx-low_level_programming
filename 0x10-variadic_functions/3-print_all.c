e015d313a9e08c43b187ac6a04ebd4179060d13f
Iyeofu <samuelojile44@gmail.com>
2022-11-18 03:45:19 -0800

    AIO

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -88,4 +88,26 @@ void print_all(const char * const format, ...)
 		{"s", print_string}
 	};
 
+	va_start(args, format);
 
+	while (format && (*(format + i)))
+	{
+		j = 0;
+
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
+		}
+
+		i++;
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
